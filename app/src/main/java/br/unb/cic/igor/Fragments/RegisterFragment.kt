acc179827bf95e00fc54c8152e83bcd9a920cd54
Fabio Marques <fabio.cmarques@hotmail.com>
2018-10-15 02:28:18 -0300

    First success on registration. Started to implement login too

---
+++ app/src/main/java/br/unb/cic/igor/Fragments/RegisterFragment.kt
@@ -0,0 +1,113 @@
+package br.unb.cic.igor.Fragments
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import br.unb.cic.igor.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_register.*
+import android.widget.Toast
+import android.app.Activity
+import android.text.TextUtils
+import android.util.Log
+import kotlinx.android.synthetic.main.fragment_register.view.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [RegisterFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class RegisterFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: LoginFragment.OnFragmentInteractionListener? = null
+
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view: View = inflater.inflate(R.layout.fragment_register, container, false)
+
+        view.register_button.setOnClickListener {
+            register(view)
+        }
+
+        // Return the fragment view/layout
+        return view
+    }
+
+    private fun register(v: View) {
+        if(!validateForm()){
+            Toast.makeText(activity, "Form not valid.", Toast.LENGTH_SHORT).show()
+        }
+
+        val email = register_email.text.toString() // email address format
+        val password = register_password.text.toString()
+
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity as Activity) { task ->
+                    Log.d("Register", "createUserWithEmail:onComplete: ${task.isSuccessful}");
+
+                    // If sign in fails, display a message to the user. If sign in succeeds
+                    // the auth state listener will be notified and logic to handle the
+                    // signed in user can be handled in the listener.
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(activity, "Auth fail.", Toast.LENGTH_SHORT).show()
+                    } else{
+                        Toast.makeText(activity, "Auth success!.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = register_email.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            register_email.error = "Required."
+            valid = false
+        } else {
+            register_email.error = null
+        }
+
+        val password = register_password.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            register_password.error = "Required."
+            valid = false
+        } else {
+            register_password.error = null
+        }
+
+        return valid
+    }
+
+
+}
--- app/src/main/java/br/unb/cic/igor/LoginActivity.kt
+++ app/src/main/java/br/unb/cic/igor/LoginActivity.kt
@@ -6,11 +6,12 @@ import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import br.unb.cic.igor.R.id.*
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 
-class LoginActivity : AppCompatActivity(), View.OnClickListener {
+class LoginActivity : AppCompatActivity() {
 
     private val RC_SIGN_IN = 23 //the request code could be any Integer
     // [START declare_auth]
@@ -20,10 +21,6 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        // Buttons
-        login_button.setOnClickListener(this)
-        create_account_button.setOnClickListener(this)
 
         // [START initialize_auth]
         mAuth = FirebaseAuth.getInstance()
@@ -38,100 +35,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         //updateUI(currentUser)
     }
 
-    private fun createAccount(email: String, password: String) {
-        Log.d(TAG, "createAccount:$email")
-        if (!validateForm()) {
-            return
-        }
 
-        showProgressDialog()
-
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = mAuth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-
-                    // [START_EXCLUDE]
-                    hideProgressDialog()
-                    // [END_EXCLUDE]
-                }
-        // [END create_user_with_email]
-    }
-
-    private fun signIn(email: String, password: String) {
-        Log.d(TAG, "signIn:$email")
-//        if (!validateForm()) {
-//            return
-//        }
-//
-//        showProgressDialog()
-
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
-                        val user = mAuth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-
-                    // [START_EXCLUDE]
-                    if (!task.isSuccessful) {
-                        status.setText(R.string.auth_failed)
-                    }
-                    hideProgressDialog()
-                    // [END_EXCLUDE]
-                }
-        // [END sign_in_with_email]
-    }
-
-    private fun validateForm(): Boolean {
-        var valid = true
-
-        val email = fieldEmail.text.toString()
-        if (TextUtils.isEmpty(email)) {
-            fieldEmail.error = "Required."
-            valid = false
-        } else {
-            fieldEmail.error = null
-        }
-
-        val password = fieldPassword.text.toString()
-        if (TextUtils.isEmpty(password)) {
-            fieldPassword.error = "Required."
-            valid = false
-        } else {
-            fieldPassword.error = null
-        }
-
-        return valid
-    }
-
-    override fun onClick(v: View) {
-        val i = v.id
-        when (i) {
-            R.id.create_account_button -> createAccount(fieldEmail.text.toString(), fieldPassword.text.toString())
-            R.id.login_button -> signIn(fieldEmail.text.toString(), fieldPassword.text.toString())
-        }
-    }
 
     companion object {
         private val TAG = "EmailPassword"
