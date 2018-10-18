825b447fb7bcecfbf6aaafdfeb0895cead320bf4
Bruno Rodrigues <brunin2509@gmail.com>
2018-10-18 00:33:05 -0300

    Created the Toolbar on the main activity

--- app/src/main/java/br/unb/cic/igor/MainActivity.kt
+++ app/src/main/java/br/unb/cic/igor/MainActivity.kt
@@ -2,11 +2,14 @@ package br.unb.cic.igor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainToolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(mainToolbar)
     }
 }
