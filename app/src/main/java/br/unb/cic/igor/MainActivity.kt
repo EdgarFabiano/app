fd8e5062994a6f56aabcc54cdb585466909f2b47
EdgarFabiano <eddy.led@gmail.com>
2018-10-18 02:17:53 -0300

    Three dots menu color

--- app/src/main/java/br/unb/cic/igor/MainActivity.kt
+++ app/src/main/java/br/unb/cic/igor/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,7 +15,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
-        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
         }
     }
@@ -26,19 +28,19 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(main_toolbar)
 
         // Initialize the action bar drawer toggle instance
-        val drawerToggle:ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
                 this,
                 drawer_layout,
                 main_toolbar,
                 R.string.drawer_open,
                 R.string.drawer_close
-        ){
-            override fun onDrawerClosed(view:View){
+        ) {
+            override fun onDrawerClosed(view: View) {
                 super.onDrawerClosed(view)
                 //toast("Drawer closed")
             }
 
-            override fun onDrawerOpened(drawerView: View){
+            override fun onDrawerOpened(drawerView: View) {
                 super.onDrawerOpened(drawerView)
                 //toast("Drawer opened")
             }
@@ -52,16 +54,13 @@ class MainActivity : AppCompatActivity() {
 
 
         // Set navigation view navigation item selected listener
-        navigation_view.setNavigationItemSelectedListener{
-            when (it.itemId){
+        navigation_view.setNavigationItemSelectedListener {
+            when (it.itemId) {
                 R.id.action_aventuras -> toast(resources.getString(R.string.aventuras))
                 R.id.action_livros -> toast(resources.getString(R.string.livros))
                 R.id.action_conta -> toast(resources.getString(R.string.conta))
                 R.id.action_notificacoes -> toast(resources.getString(R.string.notificacoes))
-                R.id.action_configuracoes ->{
-                    toast(resources.getString(R.string.configuracoes))
-                }
-
+                R.id.action_configuracoes -> toast(resources.getString(R.string.configuracoes))
             }
             // Close the drawer
             drawer_layout.closeDrawer(GravityCompat.START)
@@ -70,7 +69,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Extension function to show toast message easily
-    private fun Context.toast(message:String){
-        Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
+    private fun Context.toast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handles action bar item clicks here.
+        when (item.itemId) {
+            R.id.action_editar -> {
+                toast(resources.getString(R.string.editar))
+                return true
+            }
+            R.id.action_ordenar -> {
+                toast(resources.getString(R.string.ordenar))
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+
     }
 }
